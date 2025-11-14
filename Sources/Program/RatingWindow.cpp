77bdcee8d7dd4b70a743da40e154b2725e9082a4
HomePC\Ko <intelligencecells@gmail.com>
2025-11-14 03:39:41 +0300

    Цветные кнопки рейтинга.

--- Sources/Program/RatingWindow.cpp
+++ Sources/Program/RatingWindow.cpp
@@ -4,6 +4,7 @@
 #include <imgui.h>
 #include <Log.h>
 #include <Help.h>
+#include "AppWindow.h"
 #include "DataManager.h"
 
 DataManager::Day day;
@@ -18,6 +19,9 @@ void RatingWindow::Render() {
     ImGui::TextColored({ 0.3f, 0.6f, 0.9f, 1.f }, _dayTimeStr.c_str());
     ImGui::Separator();
 
+    const static float buttonSpaceWistrh = 200.f;
+    const float widthDescription = AppWindow::width - buttonSpaceWistrh;
+
     for (const auto& ratings : day) {
         if (_descriptions.contains(ratings.id)) {
             ImGui::Text(_descriptions[ratings.id].c_str());
@@ -26,13 +30,13 @@ void RatingWindow::Render() {
             ImGui::Text(std::to_string(ratings.id).c_str());
         }
 
-        ImGui::SameLine(200);
-        ImGui::Text(std::to_string(ratings.rate).c_str());
-        
+        ImGui::SameLine(widthDescription);
+        RateButton(ratings.rate, ratings.id);
+
         ImGui::SameLine();
-        ImGui::PushID(ratings.id);
+        ImGui::PushID(&ratings);
         if (ImGui::Button(":", ImVec2(20.f, 20.f))) {
-            LOG("TEST: set rating {}", ratings.id);
+            LOG("TEST: edit rating {}", ratings.id);
         }
         ImGui::PopID();
 
@@ -47,4 +51,20 @@ void RatingWindow::MakeUi()
 
     day = _dataManager->GetRating(dayTime);
     _descriptions = _dataManager->GetDescriptions();
+}
+
+void RatingWindow::RateButton(int rate, int id)
+{
+    const static std::array<ImVec4, 6> colorButtons = { ImVec4(1.0f, 1.0f, 1.0f, 0.05f), ImVec4(0.9f, 0.0f, 0.0f, 1.f) , ImVec4(0.9f, 0.0f, 0.9f, 1.f) , ImVec4(0.0f, 0.0f, 0.9f, 1.f), ImVec4(0.0f, 0.9f, 0.9f, 1.f), ImVec4(0.0f, 0.9f, 0.0f, 1.f) };
+
+    for (int i = 1; i <= 5; ++i) {
+        const auto& colorButton = rate == i ? colorButtons[i] : colorButtons[0];
+
+        ImGui::PushStyleColor(ImGuiCol_Button, colorButton);
+        if (ImGui::Button(TO_STRING("{}##{}", i, id).c_str(), ImVec2(20.f, 20.f))) {
+            LOG("TEST: set rating {} -> {}", rate, i);
+        }
+        ImGui::PopStyleColor();
+        ImGui::SameLine();
+    }
 }
--- Sources/Program/RatingWindow.h
+++ Sources/Program/RatingWindow.h
@@ -2,7 +2,6 @@
 
 #include <AppWindow.h>
 #include <string>
-#include <vector>
 #include <memory>
 #include <unordered_map>
 #include "ContentWindow.h"
@@ -10,16 +9,12 @@
 class DataManager;
 
 class RatingWindow : public ContentWindow {
-private:
-	struct TextData {
-		inline static const size_t size = 128;
-		char chars[size];
-	};
-
 public:
 	RatingWindow(std::shared_ptr<DataManager> data);
 	void Render() override;
+
 	void MakeUi();
+	void RateButton(int rate, int id);
 
 private:
 	std::string _dayTimeStr;
